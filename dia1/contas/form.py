1b707de0b40ab680644c29c239a6807038c85378
americogabriel <gabrieldouble007@gmail.com>
2025-09-04 16:23:03 -0300

    Revert "Django part"
    This reverts commit 998144f32824d846cfd45d658d856aee491e09df.

--- dia1/contas/form.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Transaçao
-
-
-class TransacaoForm(ModelForm):
-    class Meta:
-        model = Transaçao
-        fields = ['data','descrição','valor','categoria','observaçao']
-
